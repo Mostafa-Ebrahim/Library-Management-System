7e51afd68030ada68ae182be3460d4659ccdfb47
Mostafa Ebrahim <mostafa-ebrahim@outlook.com>
2022-09-07 10:39:53 +0200

    Add mysql database

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,12 +10,16 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
+  const imgurl = req.body.imgurl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  const product = new Product(null, title, imgurl, description, price);
+  product
+    .save()
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -41,12 +45,12 @@ exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
   const updatedPrice = req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
+  const updatedimgurl = req.body.imgurl;
   const updatedDesc = req.body.description;
   const updatedProduct = new Product(
     prodId,
     updatedTitle,
-    updatedImageUrl,
+    updatedimgurl,
     updatedDesc,
     updatedPrice
   );
