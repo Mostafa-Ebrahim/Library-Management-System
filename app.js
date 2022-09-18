e70fd45785055e88aacd7130c8967534cb299bd7
Mostafa Ebrahim <mostafa-ebrahim@outlook.com>
2022-09-18 20:24:16 +0200

    Add Authentication

--- app.js
+++ app.js
@@ -24,14 +24,14 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//     User.findByPk(1)
-//         .then(user => {
-//             req.user = user;
-//             next();
-//         })
-//         .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
